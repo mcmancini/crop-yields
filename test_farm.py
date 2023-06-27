2c26bffebfcd7d8b4499207d16c05cd75904cbe0
mcmancini <m.c.mancini@exeter.ac.uk>
2023-06-27 15:41:11 +0100

    parcel specific crop rotations in test_farm.py

--- test_farm.py
+++ test_farm.py
@@ -2,6 +2,8 @@ from cropyields.FarmManager import Farm
 import psycopg2
 from cropyields import db_parameters
 import time 
+from cropyields.config import wheat_args, maize_args, potato_args
+from cropyields.CropManager import Crop, CropRotation
   
 # List of parcel codes
 conn = psycopg2.connect(user=db_parameters['db_user'],
@@ -25,14 +27,23 @@ if conn is not None:
 OSCode = parcel_os_code[1]
 a = Farm(OSCode)
 print(a)
-args = {
-    'years': [2020, 2021],
-    'crop': 'wheat',
-    'variety': 'Winter_wheat_101',
-    'agromanagement_file': 'winter_wheat_oneyr.agro'
+parcels = a.parcel_ids
+
+# Rotation 1
+potatoes = Crop('potato', 'Potato_701', 2023, **potato_args)
+wheat = Crop('wheat', 'Winter_wheat_106', 2023, **wheat_args)
+maize = Crop('maize', 'maize_01', 2025, **maize_args)
+rotation_1 = CropRotation(potatoes, wheat, maize)
+rotation_2 = CropRotation(potatoes, wheat, maize)
+prova = {
+    parcels[0]: rotation_1,
+    parcels[1]: rotation_1,
+    parcels[2]: rotation_2
 }
+
+
 start_time = time.time()
-dt = a.run(**args)
+dt = a.run(**prova)
 end_time = time.time()
 end_time - start_time
 
@@ -69,3 +80,31 @@ print(crop2.agromanagement)
 
 rotation = CropRotation(crop1, crop2)
 print(rotation.rotation)
+
+
+
+import pandas as pd
+import datetime
+
+# Sample DataFrame with datetime index
+data = {'Value': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+index = pd.date_range(start='2023-01-01', periods=10, freq='D')
+df = pd.DataFrame(data, index=index)
+
+# List of datetime bounds
+bounds = [datetime.date(2023, 4, 1), datetime.date(2023, 11, 5), datetime.date(2025, 4, 1)]
+
+# Convert datetime bounds to pandas Timestamps
+bounds = [pd.Timestamp(bd) for bd in bounds]
+
+# Convert datetime bounds to numeric values
+ref_date = pd.Timestamp('1970-01-01')
+numeric_bounds = [(bd - ref_date).days for bd in bounds]
+
+# Split the DataFrame based on numeric bounds
+split_data = pd.cut(df.index.to_julian_date(), bins=[-float('inf')] + numeric_bounds + [float('inf')], labels=False, include_lowest=True)
+split_data = pd.Series(split_data)
+
+dfs = []
+for group in split_data.unique():
+    dfs.append(df.loc[split_data == group])
